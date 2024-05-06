729d34693850766e23de18d3c6983186537576fa
Damilarr <eaziaed@gmail.com>
2024-05-06 17:31:32 +0100

    Fixed sign in

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignIn } from "@clerk/nextjs";
 const SignInPage = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-[100vh] flex justify-center items-center">
       <SignIn path="/sign-in" fallbackRedirectUrl="/journal" />
     </div>
   );
--- app/sign-up/[[...sign-up]]/page.tsx
+++ app/sign-up/[[...sign-up]]/page.tsx
@@ -1,7 +1,7 @@
 import { SignUp } from "@clerk/nextjs";
 const SignUpPage = () => {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
+    <div className="w-full h-[100vh] flex justify-center items-center">
       <SignUp path="/sign-up" fallbackRedirectUrl="/new-user" />;
     </div>
   );
